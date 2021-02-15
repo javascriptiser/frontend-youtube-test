aba7ea054b5842253c10b89202a37de884540d08
javascriptiser <woronetskianton@gmail.com>
2021-02-15 18:33:22 +0300

    LoginForm : Показать/Скрыть пароль

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,7 +1,23 @@
 import style from './loginForm.module.css'
 import logo from '../../Assets/Images/logo.png'
+import hiddenPasswordIcon from '../../Assets/Images/hiddenPasswordIcon.svg'
+import showedPasswordIcon from '../../Assets/Images/showedPasswordIcon.svg'
+import { useEffect, useState } from 'react'
 
 export const LoginForm = () => {
+	const [state, setState] = useState({
+		passwordVisible: false,
+		type: "password"
+	});
+	const toggle = () => {
+		setState({
+			passwordVisible: !state.passwordVisible,
+			type: state.passwordVisible ? "password" : "text"
+		})
+	}
+
+
+
 	return <div className={style.container}>
 		<div className={style.container__inner}>
 			<img src={logo} alt="logo" />
@@ -12,7 +28,14 @@ export const LoginForm = () => {
 			</div>
 			<div className={style.input__wrapper}>
 				<label className={style.input__wrapperLabel} htmlFor="password">Пароль</label>
-				<input className={style.input__wrapperInput} type="password" name="password" />
+				<input className={style.input__wrapperInput}
+					type={state.type}
+					name="password" />
+				<span className={style.password}>
+					{state.passwordVisible
+						? <img src={showedPasswordIcon} alt="showedPasswordIcon" onClick={toggle} />
+						: <img src={hiddenPasswordIcon} alt="hiddenPasswordIcon" onClick={toggle} />}
+				</span>
 			</div>
 			<button className={style.btn}>Войти</button>
 		</div>
