ae8c1ced46022d4628ef323d727cde1b4438d13f
javascriptiser <woronetskianton@gmail.com>
2021-02-19 17:11:42 +0300

    Вёрстка, Рефакторинг

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -1,31 +1,41 @@
 import { useSelector } from "react-redux"
 import style from './style.module.css'
 
+const FilterPanel = ({ count, term, displayStyle }) => {
+	return <div className={style.filter__container}>
+		<div className={style.filter__containerText}>
+			Видео по запросу "{term}" <span className={style.transparent}>{count}</span>
+		</div>
+		<div className={style.filter__container_inner}>
+			<button>Grid</button>
+			<button>List</button>
+		</div>
+	</div>
+}
+
 export const SearchResult = ({ videos }) => {
 	const { count, term, displayStyle } = useSelector(state => state.appReducer)
 	return <>
-		<Template videos={videos} count={count} term={term} displayStyle={displayStyle} />
+		<FilterPanel count={count} term={term} displayStyle={displayStyle} />
+		<Template videos={videos} />
 	</>
 }
 
-const Template = ({ videos, count, term, displayStyle }) => {
+const Template = ({ videos }) => {
 	const maxCharacterCount = 40;
 	return <div className={style.container}>
-		<div>{displayStyle}</div>
-		<div>Видео по запросу "{term}" <span className={style.transparent}>{count}</span></div>
+
 		<div className={style.container__inner}>
 			{videos.map((item, index) => {
-				return <div key={index}>
-					<div>
-						<iframe
-							title={index}
-							src={`https://www.youtube.com/embed/${item.id}?showinfo=0&controls=1`}
-							frameBorder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							controls={0}
-							allowFullScreen>
-						</iframe>
-					</div>
+				return <div key={index} className={style.container__innerItem}>
+					<iframe
+						title={index}
+						src={`https://www.youtube.com/embed/${item.id}?showinfo=0&controls=1`}
+						frameBorder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						controls={0}
+						allowFullScreen>
+					</iframe>
 					<div>{item.snippet.title}</div>
 					<div className={style.transparent}>{item.snippet.description.slice(0, maxCharacterCount)}</div>
 					<div>{item.statistics.viewCount} Просмотров</div>
--- src/Components/Wrapper/Wrapper.js
+++ src/Components/Wrapper/Wrapper.js
@@ -10,8 +10,10 @@ import style from './style.module.css'
 export const Wrapper = () => {
 	const { videos, displayStyle } = useSelector(state => state.appReducer)
 	return <div className={style.container}>
-		<Header />
-		<div className={style.container__inner}>
+		<div className={style.container__header}>
+			<Header />
+		</div>
+		<div className={style.container__body}>
 			<SearchDefault />
 			{videos.length > 0 && <SearchResult videos={videos} displayStyle={displayStyle} />}
 		</div>
